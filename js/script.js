77306f16b8161bc9b203f8e068bff9906d4f41f1
Ben Basty <benbasty@yahoo.fr>
2019-09-16 14:14:36 +0800

    Travel destination website template

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// select element
+const selectElement = (s) => document.querySelector(s);
+
+// open menu on click
+selectElement('.open').addEventListener('click', ()=> {
+  selectElement('.nav-list').classList.add('active');
+});
+
+// close menu on click
+selectElement('.close').addEventListener('click', ()=> {
+  selectElement('.nav-list').classList.remove('active');
+});
